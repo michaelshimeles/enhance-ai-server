f3b4272547e5da45238f386c191f071e1ceffd71
Michael Shimeles <michaelwasihun96@gmail.com>
2023-01-04 13:49:15 -0500

    Removing hashtags from response

--- router/captions.js
+++ router/captions.js
@@ -16,6 +16,7 @@ router.get("/", async (req, res) => {
       model: "text-davinci-003",
       prompt: `I'm a social media marketer and I need your help creating Instagram captions for my client.
          I need 5 unique instagram captions with the following information in mind.
+         No need to add any hashtags.
          The product is a: ${req.query.product}
          The description of the product is: ${req.query.description}
          The tone of the captions should be: ${req.query.tone}
