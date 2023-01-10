d580158ff93ecfaab5fe6cedfd9b7af7c3e74c94
Michael Shimeles <michaelwasihun96@gmail.com>
2023-01-10 18:01:01 -0500

    Cors error on site has been fix

--- server.js
+++ server.js
@@ -10,18 +10,24 @@ const grammarRouter = require("./router/grammar");
 const captionsRouter = require("./router/captions");
 const genericCaptionRouter = require("./router/generic");
 
-app.use(function (_req, res, next) {
-  res.header("Access-Control-Allow-Origin", [
-    "http://localhost:3000/",
-    "https://enhanceai.me/",
-  ]);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://enhanceai.me/');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
   next();
 });
-
 app.use(express.json());
 
 app.get("/", (_req, res) => {
