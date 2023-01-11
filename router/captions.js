e8d489cd1948d2d6745acc25fb9267475241164f
Michael Shimeles <michaelwasihun96@gmail.com>
2023-01-11 13:17:39 -0500

    Added hashtag generator

--- router/captions.js
+++ router/captions.js
@@ -14,9 +14,9 @@ router.get("/", async (req, res) => {
   try {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `I'm a social media marketer and I need your help creating Instagram captions for my client.
+      prompt: `You are a social media manager at the top Marketing Agency and I need your help creating Instagram captions for a client.
          I need 5 different captions regarding: ${req.query.description}
-         Please don't make the captions too short
+         Please don't make the captions too short and don't add any hashtags.
         `,
       temperature: 1,
       max_tokens: 256,
@@ -34,7 +34,7 @@ router.get("/generic", async (req, res) => {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `You are a social media manager at the top Marketing Agency. You create ${req.query.platform} captions. 
-         I need you to create a 5 different captions with a ${req.query.tone} tone keeping the following information in mind: ${req.query.description}.`,
+         I need you to create a 5 different captions with a ${req.query.tone} tone. Here's the information I want you to use to create captions with: ${req.query.description}.`,
       temperature: 1,
       max_tokens: 256,
     });
@@ -44,5 +44,21 @@ router.get("/generic", async (req, res) => {
   }
 });
 
+router.get("/hashtags", async (req, res) => {
+  const openai = new OpenAIApi(configuration);
+
+  console.log(req.query)
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `I need you to generate 10 instagram hashtags for a post I'm creating. My post is in regards to the ${req.query.niche} niche`,
+      temperature: 0,
+      max_tokens: 256,
+    });
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
